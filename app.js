08ca347e8a99a7cd4c48b2c1b6109ae98e46d481
Ben Greanya <bengreanya@gmail.com>
2022-10-25 16:38:10 -0700

    add button listener

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const nameInput = document.getElementById('name-input');
 console.log(nameInput);
+const buttonInput = document.getElementById('buttonInput');
+buttonInput.addEventListener('click', () => {
+    console.log(buttonInput, "I'm clicking the button");
+});
